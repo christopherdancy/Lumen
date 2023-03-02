d307ea9aa886e6efa0a8cbb6ff5ec69977d8db15
Christopher Dancy <chris.dancy@decentlabs.io>
2023-03-02 11:12:26 -0700

    updated tests

--- test/PoA_test.ts
+++ test/PoA_test.ts
@@ -1,37 +1,45 @@
 // import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 // import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import chai from "chai";
 import { ethers } from "hardhat";
 
-import {solidity} from "ethereum-waffle";
+// import { solidity } from "ethereum-waffle";
 import { ProofofAccess } from "../typechain-types";
 
-chai.use(solidity);
+// chai.use(solidity);
 
 describe("SigTest", () => {
-    let poaContract: ProofofAccess;
+  let poaContract: ProofofAccess;
 
-    beforeEach(async () => {
-        // Get eth signers
-        const signers = await ethers.getSigners();
-        // prepare the contract for deployment
-        const counterFactory = await ethers.getContractFactory(
-            "ProofofAccess",
-            signers[0]
+  beforeEach(async () => {
+    // Get eth signers
+    const signers = await ethers.getSigners();
+    // prepare the contract for deployment
+    const counterFactory = await ethers.getContractFactory(
+      "ProofofAccess",
+      signers[0]
+    );
+
+    // DEPLOY
+    poaContract = (await counterFactory.deploy()) as ProofofAccess;
+    await poaContract.deployed();
+  });
+
+  describe("test", async () => {
+    it("should request and attest", async () => {
+      const [requestor, attestor] = await ethers.getSigners();
+
+      await expect(poaContract.connect(requestor).request(attestor.address))
+        .to.emit(poaContract, "Requested")
+        .withArgs(requestor.address, attestor.address, 1);
+      await expect(poaContract.connect(attestor).attest(requestor.address))
+        .to.emit(poaContract, "Attested")
+        .withArgs(
+          requestor.address,
+          attestor.address,
+          1,
+          (await (await ethers.provider.getBlock("latest")).timestamp) + 1
         );
-
-        // DEPLOY
-        poaContract = (await counterFactory.deploy()) as ProofofAccess;
-        await poaContract.deployed();
     });
-
-    describe("test", async () => {
-        it("should request and attest", async () => {
-            const [requestor, attestor] = await ethers.getSigners();
-
-            await expect(poaContract.connect(requestor).request(attestor.address)).to.emit(poaContract, 'Requested').withArgs(requestor.address, attestor.address, 1);
-            await expect(poaContract.connect(attestor).attest(requestor.address)).to.emit(poaContract, 'Attested').withArgs(requestor.address, attestor.address, 1, (await (await ethers.provider.getBlock("latest")).timestamp + 1));
-        });
-    });
+  });
 });
