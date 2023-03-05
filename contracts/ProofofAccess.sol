75fc2c8e76f87a9c4727fc28d83e3270060fbbb4
Christopher Dancy <chris.dancy@decentlabs.io>
2023-03-04 23:03:27 -0700

    added AttestationStation

--- contracts/ProofofAccess.sol
+++ contracts/ProofofAccess.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./mocks/AttestationStation.sol";
 
 contract ProofofAccess is ERC721 {
     using Counters for Counters.Counter;
-    IERC20 public usdc;
+    AttestationStation private attestationStatation;
 
     Counters.Counter private _tokenIdCounter;
 
@@ -29,7 +30,9 @@ contract ProofofAccess is ERC721 {
         uint256 timestamp
     );
 
-    constructor() ERC721("SoulAttestedPortfolio", "SAP") {}
+    constructor(address ats) ERC721("SoulAttestedPortfolio", "SAP") {
+        attestationStatation = AttestationStation(ats);
+    }
 
     function request(address attester) public returns (uint256 tokenId) {
         _tokenIdCounter.increment();
@@ -39,12 +42,13 @@ contract ProofofAccess is ERC721 {
     }
 
     // include self attestions + when address was provided
-    function attest(address requestor) public {
+    function attest(address requestor, AttestationStation.AttestationData[] memory attests) public {
         uint256 tokenId = requests[requestor][msg.sender];
         // Check if this is a self-attestion
         if (requestor == msg.sender && tokenId == 0)
             tokenId = request(msg.sender);
         require(tokenId != 0, "Request Does Not Exist");
+        attestationStatation.attest(attests);
 
         // If token does not exist mint + setTokenURI
         if (!_exists(tokenId)) _safeMint(msg.sender, tokenId);
@@ -59,12 +63,20 @@ contract ProofofAccess is ERC721 {
         );
     }
 
-    function myAttestData(address attestor) public view returns(uint256[] memory) {
+    function myAttestData(address attestor)
+        public
+        view
+        returns (uint256[] memory)
+    {
         return attestations[attestor];
     }
 
-    function tokenIdData(address requestor, address attestor) public view returns(uint256) {
-        uint tokenId = requests[requestor][attestor];
+    function tokenIdData(address requestor, address attestor)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 tokenId = requests[requestor][attestor];
         return tokenPortfolios[tokenId].timestamp;
     }
 
--- test/PoA_test.ts
+++ test/PoA_test.ts
@@ -4,24 +4,33 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 // import { solidity } from "ethereum-waffle";
-import { ProofofAccess } from "../typechain-types";
+import { AttestationStation, ProofofAccess } from "../typechain-types";
 
 // chai.use(solidity);
 
 describe("SigTest", () => {
   let poaContract: ProofofAccess;
+  let attestContract: AttestationStation;
 
   beforeEach(async () => {
     // Get eth signers
     const signers = await ethers.getSigners();
     // prepare the contract for deployment
+    const attestationFactory = await ethers.getContractFactory(
+      "AttestationStation",
+      signers[0]
+    );
     const counterFactory = await ethers.getContractFactory(
       "ProofofAccess",
       signers[0]
     );
 
     // DEPLOY
-    poaContract = (await counterFactory.deploy()) as ProofofAccess;
+    attestContract = (await attestationFactory.deploy()) as AttestationStation;
+    poaContract = (await counterFactory.deploy(
+      attestContract.address
+    )) as ProofofAccess;
+    await attestContract.deployed();
     await poaContract.deployed();
   });
 
@@ -32,7 +41,15 @@ describe("SigTest", () => {
       await expect(poaContract.connect(requestor).request(attestor.address))
         .to.emit(poaContract, "Requested")
         .withArgs(requestor.address, attestor.address, 1);
-      await expect(poaContract.connect(attestor).attest(requestor.address))
+      await expect(
+        poaContract.connect(attestor).attest(requestor.address, [
+          {
+            about: attestor.address,
+            key: ethers.utils.formatBytes32String("test"),
+            val: ethers.utils.formatBytes32String("test"),
+          },
+        ])
+      )
         .to.emit(poaContract, "Attested")
         .withArgs(
           requestor.address,
@@ -41,7 +58,15 @@ describe("SigTest", () => {
           (await (await ethers.provider.getBlock("latest")).timestamp) + 1
         );
       // self attestation
-      await expect(poaContract.connect(attestor).attest(attestor.address))
+      await expect(
+        poaContract.connect(attestor).attest(attestor.address, [
+          {
+            about: attestor.address,
+            key: ethers.utils.formatBytes32String("test"),
+            val: ethers.utils.formatBytes32String("test"),
+          },
+        ])
+      )
         .to.emit(poaContract, "Attested")
         .withArgs(
           attestor.address,
